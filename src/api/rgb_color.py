0a337bf97d4a7d329bc96987f53465002fe8c775
陆超 <deerchao@live.com>
2021-02-19 10:29:32 +0800

    随机颜色算法，分别随机R/G/B分量

--- src/api/rgb_color.py
+++ src/api/rgb_color.py
@@ -1,7 +1,21 @@
 import random as rnd
 
-def random(min = 0, max = 0xffffff):
-    return rnd.randint(min, max)
+
+def random(min=0, max=0xffffff):
+    minRed = (min & 0xff0000) >> 16
+    minGreen = (min & 0xff00) >> 8
+    minBlue = (min & 0xff)
+
+    maxRed = (max & 0xff0000) >> 16
+    maxGreen = (max & 0xff00) >> 8
+    maxBlue = (max & 0xff)
+
+    red = rnd.randint(minRed, maxRed)
+    green = rnd.randint(minGreen, maxGreen)
+    blue = rnd.randint(minBlue, maxBlue)
+
+    color = (int(red) << 16) + (int(green) << 8) + int(blue)
+    return color
 
 
 def average(colors):
@@ -12,15 +26,15 @@ def average(colors):
     red = 0
     green = 0
     blue = 0
-    
+
     for color in colors:
         red += (color & 0xff0000) >> 16
         green += (color & 0xff00) >> 8
         blue += (color & 0xff)
-    
+
     red //= count
     green //= count
-    blue //=count
+    blue //= count
 
-    average =  (int(red) << 16) + (int(green) << 8) + int(blue)
+    average = (int(red) << 16) + (int(green) << 8) + int(blue)
     return average
